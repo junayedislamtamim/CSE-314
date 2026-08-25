db4c707a1fc341b2e00f79d82db2d1907ba3e6f9
junayedislamtamim <junayedislamtamim8@gmail.com>
2026-08-25 20:55:08 +0600

    IPC assignment initialization

---
+++ offline-3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/log.txt", "w", stdout);
+    int N, M, x, y;
+    cin >> N >> M >> x >> y;
+
+
+}
